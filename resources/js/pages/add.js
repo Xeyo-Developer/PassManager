adaf175d02b400a321be59313ca95fd35115dc6e
Xeyo <102188054+Xeyo-Developer@users.noreply.github.com>
2025-05-05 23:25:57 +0200

    Added list of passwords and their addition

---
+++ resources/js/pages/add.js
@@ -0,0 +1,33 @@
+const addPasswordButton = document.getElementById("addPassword");
+
+const errorsContainer = document.getElementById("errors");
+const usernameError = document.getElementById("usernameError");
+const passwordError = document.getElementById("passwordError");
+const closeErrors = document.getElementById("closeErrors");
+
+if (addPasswordButton) {
+    addPasswordButton.addEventListener("click", () => {
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        const selectedType = document.querySelector('input[name="type"]:checked').value;
+
+        if (!password || !username) {
+            usernameError.classList[!username ? "remove" : "add"]("hidden");
+            passwordError.classList[!password ? "remove" : "add"]("hidden");
+            errorsContainer.classList.remove("hidden");
+            return;
+        }
+
+        window.electron.addPassword({
+            username, password, type: selectedType
+        });
+
+        window.location.href = "../views/list.html";
+    });
+}
+
+if (closeErrors) {
+    closeErrors.addEventListener("click", () => {
+        errorsContainer.classList.add("hidden");
+    });
+}
